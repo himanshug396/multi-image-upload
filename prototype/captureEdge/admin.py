234f586f474a1784551062b15022d73a273ac977
Himanshu Gupta <himanshugupta.396@gmail.com>
2020-03-16 23:53:08 +0530

    models

---
+++ prototype/captureEdge/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Claim, Phone, Photo
+
+admin.site.register(Claim)
+admin.site.register(Phone)
+admin.site.register(Photo)
+# Register your models here.
---
+++ prototype/captureEdge/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Claim(BaseModel):
+    id =  models.AutoField(primary_key=True, unique=True)
+    description = models.CharField(max_length=200)
+    def __str__(self):
+        return str(self.id) + '_' + self.description
+
+class Phone(BaseModel):
+    id = models.AutoField(primary_key=True)
+    claim_id = models.ForeignKey(Claim, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=False)
+    phone = PhoneNumberField(null=False, blank=False)
+    def __str__(self):
+        return str(self.claim_id) + '_' + self.name + '_' + str(self.phone)
+
+class Photo(BaseModel):
+    id = models.AutoField(primary_key=True)
+    claim_id = models.ForeignKey(Claim, on_delete=models.CASCADE)
+    phone = models.ForeignKey(Phone, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    def __str__(self):
+        return str(self.phone) + '_' + str(self.id)
