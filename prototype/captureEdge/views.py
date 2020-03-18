67e01293238beb166d1a82d3780f8638b3d3962a
Himanshu Gupta <himanshugupta.396@gmail.com>
2020-03-18 17:15:01 +0530

    multipe image upload

--- prototype/captureEdge/views.py
+++ prototype/captureEdge/views.py
@@ -4,6 +4,9 @@ from .models import *
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils import timezone
 from .forms import ImageForm
+from django.http import JsonResponse
+from django.views import View
+import time
 
 def homepage(request):
     claims = Claim.objects.all()
@@ -27,51 +30,21 @@ def user_details(request):
 		#image_url send to the person on his phone
 	return HttpResponse("success: " + image_url)
 
-# def upload_form(request, claim_id, phone):
-# 	request.session['claim_id'] = claim_id
-# 	request.session['phone'] = phone
-# 	# if request.method == 'POST':
-# 	# 	form_create = ImageForm(request.POST or None, request.FILES or None,)
-# 	# 	if form_create.is_valid():
-# 	# 		for field in request.FILES.keys():
-# 	# 			for formfile in request.FILES.getlist(field):
-# 	# 				img = Photo(photo=formfile)
-# 	# 				img.save()
-# 	# 				print(img)
-# 	return render(request, 'captureEdge/upload.html')
-
-
-# def upload_images(request):
-# 	claim_id = request.session['claim_id']
-# 	phone = request.session['phone']
-# 	if request.method == 'POST':
-# 		print(request.FILES('pro-image'))
-# 		for field in request.FILES.keys():
-# 			for formfile in request.FILES.getlist(field):
-# 				img = Photo(claim_id=claim_id, phone=phone, photo=formfile)
-# 				img.save()
-# 				print(img)	
-# 	print(claim_id)
-# 	print(phone)
-	
-# 	return HttpResponse("success: ")
 
 def upload_form(request, claim_id, phone):
 	if request.method == 'POST':
+		time.sleep(1)  # You don't need this line. This is just to delay the process so you can see the progress bar testing locally.
 		form = ImageForm(request.POST or None, request.FILES or None,)
 		if form.is_valid():
 			for field in request.FILES.keys():
 				for formfile in request.FILES.getlist(field):
 					claim_obj = Claim.objects.get(pk=int(claim_id))
 					claim_obj.photo_set.create(phone=phone, photo=formfile)
-					# claim_obj.phone_id = str(phone)
-					# claim_obj['phone'] = phone
 					claim_obj.save()
-					# img = Photo(claim_id=claim_id, phone=phone, photo=formfile)
-					# img.save()
-					# print(img)
-			return HttpResponse('success') 
+					data = {'is_valid': True}
+		else:
+			data = {'is_valid': False}
+		return JsonResponse(data)
 	else:
-		print('else')
-		form = ImageForm()
-	return render(request, 'captureEdge/upload.html', {'form': form})
+		form =  ImageForm()
+	return render(request, 'captureEdge/upload.html', {'form': form, 'claim_id': claim_id, 'phone': phone})
